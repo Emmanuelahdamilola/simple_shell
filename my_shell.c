416a44a413cb57b2e8a02816438688f535a21439
Magret <oyedeleabiodun79@gmail.com>
2023-06-15 20:50:14 -0700

    simple shell

--- my_shell.c
+++ my_shell.c
@@ -2,12 +2,33 @@
 
 /**
  * main - prints a prompt
+ * @argc: argument count
+ * @argv: strings
  *
  * Return: 0 (success)
  */
 
-int main(void)
+int main(int argc, char **argv)
 {
-	_puts("$ ");
+	char *getline_ptr;
+	size_t n = 0;
+	ssize_t num_char;
+
+	(void)argc;
+	(void)argv;
+
+	while (1)
+	{
+		_puts("$ ");
+		num_char = getline(&getline_ptr, &n, stdin);
+		if (num_char == -1)
+		{
+			_puts("<<<Can't be found>>>");
+			return (-1);
+		}
+		_puts(getline_ptr);
+		_puts("\n");
+		free(getline_ptr);
+	}
 	return (0);
 }
