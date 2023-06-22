b36361a3092c1d2b0e7adc3374456f104f55efa0
Magret <oyedeleabiodun79@gmail.com>
2023-06-22 11:00:20 -0700

    added comment to the getenv function

--- _getenv.c
+++ _getenv.c
@@ -1,8 +1,15 @@
 #include "main.h"
 
+/**
+ * _getenv - gets environment variable
+ * @name: environment variable to get
+ *
+ * Return: environment variable
+ */
+
 char *_getenv(const char *name)
 {
-       	char **env = environ;
+	char **env = environ;
 	char *equal;
 	size_t length;
 
@@ -18,6 +25,7 @@ char *_getenv(const char *name)
 				return (equal + 1);
 		}
 		env++;
+		/*printf("%s", *env);*/
 	}
 	return (NULL);
 }
