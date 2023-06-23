9a658424e253a6d555f19e5026803032cbc648aa
Emmanuelahdamilola <emmanuelahdamilola1992@gmail.com>
2023-06-22 17:40:10 -0700

    implemented exit builtin and env

--- location.c
+++ location.c
@@ -1,81 +1,95 @@
 #include "main.h"
+
 /**
  * location - get file path
  * @command: command from stdin
- * Return: file pth
+ * @argv: argument vector
+ * Return: file path
  */
-char *location(char *command)
+char *location(char *command, char **argv)
 {
 	struct stat buffer;
-	char *path, *path_copy, *path_token, *file_path;
-	size_t file_path_len;
-
-	if (_strcmp(command, "env") == 0)
-	{
-		env();
-		return (NULL);
-	}
+	char *file_path;
 
 	if (stat(command, &buffer) == 0)
 		return (command);
-	path = getenv("PATH");
-	if (path != NULL)
+
+	if (_strcmp(command, "exit") == 0 && argv[1] != NULL)
 	{
-		path_copy = _strdup(path);
-		path_token = strtok(path_copy, ":");
-
-		while (path_token != NULL)
-		{
-			file_path_len = _strlen(path_token) + 1 + _strlen(command) + 1;
-			file_path = malloc(file_path_len);
-			_strcpy(file_path, path_token);
-			_strcat(file_path, "/");
-			_strcat(file_path, command);
-
-			if (access(file_path, X_OK) == 0)
-			{
-				free(path_copy);
-				return (file_path);
-			}
-			free(file_path);
-			path_token = strtok(NULL, ":");
-		}
-		free(path_copy);
+		int status = atoi(argv[1]);
+		exit(status);
 	}
+	if (_strcmp(command, "env") == 0)
+	{
+		char **envptr;
+
+		envptr = environ;
+		while (*envptr != NULL)
+		{
+			write(STDOUT_FILENO, *envptr, _strlen(*envptr));
+			write(STDOUT_FILENO, "\n", 1);
+			envptr++;
+		}
+	}
+
+	file_path = search_file_path(command);
+	return (file_path);
+}
+
+/**
+ * search_file_path - function that searches for the file
+ * in the PATH environment variable
+ * @command: command to search for
+ * Return: file path if found, otherwise NULL
+ */
+char *search_file_path(char *command)
+{
+	char *path;
+	char *path_copy, *path_token;
+	char *file_path;
+
+	path = getenv("PATH");
+	if (path == NULL)
+		return (NULL);
+
+	path_copy = _strdup(path);
+	path_token = strtok(path_copy, ":");
+	while (path_token != NULL)
+	{
+		file_path = construct_filepath(path_token, command);
+		if (access(file_path, X_OK) == 0)
+		{
+			free(path_copy);
+			return (file_path);
+		}
+		free(file_path);
+		path_token = strtok(NULL, ":");
+	}
+	free(path_copy);
 	return (NULL);
 }
 
 /**
- * env - function that print environment variables and their value
- * Description: function that  effectively print each environment variable
- * followed by a newline character, replicating the
- * behavior of the env command.
- * Return: Null
+ * construct_filepath - function that construct the file path
+ * by combining directory and command
+ * @directory: directory from the PATH
+ * @command: command to append to the directory
+ * Return: constructed file path
  */
-
-void env(void)
+char *construct_filepath(char *directory, char *command)
 {
-	char **envptr;
-	int env_count;
-	char *env_string;
-	size_t len_str;
+	size_t file_path_len;
+	char *file_path;
 
-	envptr = environ;
-	env_count = 0;
+	file_path_len = _strlen(directory) + 1 + _strlen(command) + 1;
+	file_path = malloc(file_path_len);
 
-	while (*envptr != NULL)
-	{
-		len_str = _strlen(*envptr) + 1;
-		env_string = malloc(len_str);
+	if (file_path == NULL)
+		return NULL;
 
-		if (env_string != NULL)
-		{
-			_strcpy(env_string, *envptr);
-			_strcat(env_string, "\n");
-			write(STDOUT_FILENO, env_string, len_str);
-			free(env_string);
-		}
-		envptr++;
-		env_count++;
-	}
+	_strcpy(file_path, directory);
+	_strcat(file_path, "/");
+	_strcat(file_path, command);
+	return (file_path);
 }
+
