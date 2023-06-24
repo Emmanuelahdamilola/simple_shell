42ce2fa8c8a04b71d3045bd93ee1798a9a4a3aac
Magret <oyedeleabiodun79@gmail.com>
2023-06-23 18:59:59 -0700

    modified main function

--- main.c
+++ main.c
@@ -33,22 +33,6 @@ void read_execute_loop(void)
 }
 
 /**
- * free_argv - Free the allocated memory for the argument array.
- * @argv: The argument array.
- * @size: The size of the argument array.
- * Return: void
- */
-void free_argv(char **argv, int size)
-{
-	int i;
-
-	for (i = 0; i < size; i++)
-		free(argv[i]);
-
-	free(argv);
-}
-
-/**
  * main - Building shell to imitate bash.
  * @argc: Count of arguments.
  * @argv: Arguments.
