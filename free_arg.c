ef396a4eacc259c7ef9d96f86bc063cd72d18728
Magret <oyedeleabiodun79@gmail.com>
2023-06-23 19:00:48 -0700

    free argv function

---
+++ free_arg.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * free_argv - Free the allocated memory for the argument array.
+ * @argv: The argument array.
+ * @size: The size of the argument array.
+ * Return: void
+ */
+void free_argv(char **argv, int size)
+{
+	int i;
+
+	for (i = 0; i < size; i++)
+		free(argv[i]);
+
+	free(argv);
+}
